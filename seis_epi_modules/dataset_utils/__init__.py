001c7ddb2a5a373381b67c16257747d03ee5c40f
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-04-27 09:27:19 +0000

    update rw method

--- seis_epi_modules/dataset_utils/__init__.py
+++ seis_epi_modules/dataset_utils/__init__.py
@@ -1,3 +1,3 @@
 from .read_tfr import read_tfr
 from .data_operation import *
-from .write_tfr import *
+from .write_tfr import write_tfr
--- seis_epi_modules/dataset_utils/read_tfr.py
+++ seis_epi_modules/dataset_utils/read_tfr.py
@@ -1,28 +1,25 @@
 import tensorflow as tf
 
-@tf.autograph.experimental.do_not_convert
-def read_tfr(inFile__, with_year__):
-  def decoder(n):
+''' (personal note)
     keys = ['data', 'aavg_ratio', 'overall_ratio', 'magn', 'evla',
             'evlo', 'stla', 'stlo', 'dist',
             'filename', 'start']
             
     dtyp = [tf.float32 for _ in range(9)] + [tf.string for _ in range(2)]
-    
-    if with_year__:
-      keys += ['year']
-      dtyp += [tf.int32]
-
+'''
+@tf.autograph.experimental.do_not_convert
+def read_tfr(inFile__, keys__, dtyp__):
+  def decoder(n):
     configs = {
         key: tf.io.FixedLenFeature([], tf.string)
-        for key in keys
+        for key in keys__
     }
 
     parseSingle = tf.io.parse_single_example(n, configs)
 
     dataStructure = {
-        key: tf.io.parse_tensor(parseSingle[key], dtyp[i])
-        for i, key in enumerate(keys)
+        key: tf.io.parse_tensor(parseSingle[key], dtyp__[i])
+        for i, key in enumerate(keys__)
     }
     return (dataStructure)
   return tf.data.TFRecordDataset(inFile__).map(decoder)
