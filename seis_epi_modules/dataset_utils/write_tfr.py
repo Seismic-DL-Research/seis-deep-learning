3540969cf0ae1ea855dd224dbdbb8d6f5a7386d5
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-04-27 09:03:24 +0000

    update rw method

--- seis_epi_modules/dataset_utils/write_tfr.py
+++ seis_epi_modules/dataset_utils/write_tfr.py
@@ -8,23 +8,33 @@ def featurize(value__):
       bytes_list = tf.train.BytesList(value=[value__.numpy()])
   )
 
-def write_tfr(ds__, outFile__):
-  keys = ['data', 'aavg_ratio', 'overall_ratio', 'magn', 'evla',
-          'evlo', 'stla', 'stlo', 'dist',
-          'filename', 'start', 'year']
+# keys = ['data', 'aavg_ratio', 'overall_ratio', 'magn', 'evla',
+#           'evlo', 'stla', 'stlo', 'dist',
+#           'filename', 'start', 'year']
 
-  dtyp = [tf.float32 for _ in range(9)] + [tf.string for _ in range(2)] + [tf.int32]
-
-  with tf.io.TFRecordWriter(f'{outFile__}') as f:
-    serialized = {}
+# dtyp = [tf.float32 for _ in range(9)] + [tf.string for _ in range(2)] + [tf.int32]
+def write_tfr_from_dataset(ds__, keys__, dtyp__, out_file__):
+  with tf.io.TFRecordWriter(f'{out_file__}') as f:
 
     for dsElement in ds__.take(-1):
       collective = tf.train.Features(
           feature = {
-              keys[i]: featurize(serialize(dsElement[dsKey], dtyp[i]))
+              keys__[i]: featurize(serialize(dsElement[dsKey], dtyp__[i]))
               for i, dsKey in enumerate(dsElement)
           }
       )
       record_bytes = tf.train.Example(features=collective).SerializeToString()
 
       f.write(record_bytes)
+
+def write_tfr_from_tensor(list_tensor__, keys__, dtyp__, out_file__):
+  collective = tf.train.Features(
+    feature={
+      tkey: featurize(serialize(list_tensor__[i], dtyp__[i]))
+      for i, tkey in enumerate(keys__)
+    }
+  )
+  record_bytes = tf.train.Example(features=collective).SerializeToString()
+
+  with tf.io.TFRecordWriter(f'{out_file__}') as f:
+    f.write(record_bytes)
