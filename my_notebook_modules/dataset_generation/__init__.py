ca0703451aa8c0ce4b86aad2f201d180fa9927fa
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-04-27 15:15:10 +0000

    adding modules to dataset_generation

--- my_notebook_modules/dataset_generation/__init__.py
+++ my_notebook_modules/dataset_generation/__init__.py
@@ -3,3 +3,4 @@ from .fetch_extract_and_list import fetch_extract_and_list
 from .statistics import stalta
 from .statistics import power
 from .decisor import decisor
+from .clip_event import clip_event
