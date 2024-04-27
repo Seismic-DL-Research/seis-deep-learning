9eca4f4d6f701ad676db2c378e3ef0a6756acd8f
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-04-27 23:42:43 +0000

    refining tfrecords I/O

--- my_notebook_modules/dataset_utils/io/write_tfr.py
+++ my_notebook_modules/dataset_utils/io/write_tfr.py
@@ -20,7 +20,7 @@ def get_keys_and_types(keys__):
   return keys, keys_type
 
 def write_tfr_from_dataset(ds__, keys__, batch_size__, 
-                           take_size__, out_file__):
+                           take_size__, out_file__, batches_per_file__=-1):
   '''
     write_tfr_from_dataset:
       * Used to write tensorflow binary record files from a dataset.
@@ -42,24 +42,43 @@ def write_tfr_from_dataset(ds__, keys__, batch_size__,
         The size of total batch taken from "ds__" dataset.
       * out_file__ <string> [0]
         The path of the output file 
+      * batches_per_file__ <int> [0] {optional}
+        Used for sharding. The value tells how many batches are recorded per file.
+        Total data elements (singular data) is batches_per_file__ * batch_size__
+        DEFAULT = -1 (take all batches)
 
     return:
       * (0) <0> [0]
         returns nothing.
   '''
   keys, keys_type = get_keys_and_types(keys__)
+  init_filename = (out_file__ if batches_per_file__ == -1 
+                  else f'{".".join(out_file__.split(".")[:-1])}:0.tfr')
+  f = tf.io.TFRecordWriter(init_filename)
+  batch_rounds = 0
 
-  with tf.io.TFRecordWriter(f'{out_file__}') as f:
-    for dsElement in ds__.batch(batch_size__).take(take_size__):
-      collective = tf.train.Features(
-          feature = {
-              key: featurize(serialize(dsElement[key], keys_type[i]))
-              for i, key in enumerate(keys)
-          }
-      )
-      record_bytes = tf.train.Example(features=collective).SerializeToString()
+  for dsElement in ds__.batch(batch_size__).take(take_size__):
+    # close the current file and open the new file if batches_per_file__ is
+    # enabled (!=1).
+    if batches_per_file__ != -1 and batch_rounds % batches_per_file__ == 0:
+      f.close()
+      f = tf.io.TFRecordWriter(
+        f'{".".join(out_file__.split(".")[:-1])}:{int(batch_rounds/batches_per_file__)}.tfr')
 
-      f.write(record_bytes)
+    collective = tf.train.Features(
+        feature = {
+            key: featurize(serialize(dsElement[key], keys_type[i]))
+            for i, key in enumerate(keys)
+        }
+    )
+    # write serialized tensor (in bytes).
+    record_bytes = tf.train.Example(features=collective).SerializeToString()
+    f.write(record_bytes)
+
+    # increase the batch_epoch number.
+    batch_rounds += 1
+
+  f.close()
 
 def write_tfr_from_list(list_tensor__, keys__, out_file__):
   '''
