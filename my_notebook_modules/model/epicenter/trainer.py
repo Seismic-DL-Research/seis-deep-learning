66d87b773c401dfe01fb1d4fae83b120e51d5aca
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-04-28 16:18:04 +0000

    adding gradient taping

--- my_notebook_modules/model/epicenter/trainer.py
+++ my_notebook_modules/model/epicenter/trainer.py
@@ -1,8 +1,19 @@
 import tensorflow as tf
+import my_notebook_modules as mynbm
 
 def trainer(model__, train_dataset__, opt__, batch_size__, epoch__):
   for epoch in range(epoch__):
     for train_dataset in train_dataset__.batch(batch_size__).take(-1):
       with tf.GradientTape() as g:
-        y_hat = model__(train_dataset['data'][:,:])
+        # get model's epicentral distance estimation values
+        y_hat = model__(train_dataset['data'][:,:,:,:50,:])
+        y = tf.expand_dims(train_dataset['dist'], axis=0)
+        
+        # calculating the loss
+        loss = mynbm.model.epicenter.mae(y_hat, y)
+
+        # apply gradient descent to update weights
+        grad = g.gradient(loss, model__.trainable_vars)
+        opt__.apply_gradients(zip(grad, model__.trainable_vars))
+      print('Loss: ', loss)
   pass
