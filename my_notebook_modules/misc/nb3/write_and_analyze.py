e28f030dfe62f92db6045ef99337386688954c11
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-04-30 16:06:38 +0000

    fixed missing keys

--- my_notebook_modules/misc/nb3/write_and_analyze.py
+++ my_notebook_modules/misc/nb3/write_and_analyze.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import tensorflow.math as tfm
 import my_notebook_modules as mynbm
+from tqdm import tqdm
 
 def write_and_analyze(ds__, out_file__, batch_size__, 
                       take_size__, cum_batch__, keys__):
@@ -9,6 +10,8 @@ def write_and_analyze(ds__, out_file__, batch_size__,
   cum_data = [0 for _ in keys__]
   cum_batch = 0
   write_incidents = 0
+  bar = tqdm(total=1, position=0, bar_format='[{elapsed}] {desc}')
+
   for m, data_elem in enumerate(ds__.batch(batch_size__, drop_remainder=True)
                                 .take(take_size__)):
     for i, stat in enumerate(mynbm.misc.nb3.get_stat(data_elem['data'])): 
@@ -22,7 +25,8 @@ def write_and_analyze(ds__, out_file__, batch_size__,
         cum_data[i] = tf.concat([cum_data[i], data_elem[key]], axis=0)
 
     cum_batch += 1
-
+    bar.set_description_str(f'Batch rounds: {cum_batch}')
+    
     if cum_batch %  cum_batch__ == 0:
       write_incidents += 1
       mynbm.dataset_utils.io.write_tfr_from_list(
@@ -35,7 +39,7 @@ def write_and_analyze(ds__, out_file__, batch_size__,
     mynbm.dataset_utils.io.write_tfr_from_list(
       cum_data, 
       keys__, 
-      out_file__ + f'-{write_incidents}.tfr')
+      out_file__ + f'-{write_incidents+1}.tfr')
 
   for i, cum in enumerate(cums[:-1]):
     my_cum = tf.convert_to_tensor(cum)
@@ -45,9 +49,9 @@ def write_and_analyze(ds__, out_file__, batch_size__,
     analysis_matrix[i].append(tfm.reduce_mean(my_cum, axis=1))
     analysis_matrix[i].append(tfm.reduce_std(my_cum, axis=1))
     
-  analysis_matrix = [analysis_matrix]
+  analysis_matrix = [analysis_matrix, cums[-1]]
   mynbm.dataset_utils.io.write_tfr_from_list(analysis_matrix,
-                                            ['analysis.f32'],
+                                            ['analysis.f32', 'dist.f32'],
                                              out_file__ + '-analysis.tfr')
-
+  bar.close()
   return tf.convert_to_tensor(analysis_matrix[0])
