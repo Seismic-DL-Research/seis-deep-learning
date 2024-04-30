51572ab610670670309c6bc6642841422e03947e
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-04-30 14:59:01 +0000

    fixing miscs

--- my_notebook_modules/__init__.py
+++ my_notebook_modules/__init__.py
@@ -2,6 +2,5 @@ from . import layers
 from . import dataset_utils
 from . import dataset_generation
 from . import model
-from . import plot
 from . import misc
 from .cwd import *
--- my_notebook_modules/misc/nb3/write_and_analyze.py
+++ my_notebook_modules/misc/nb3/write_and_analyze.py
@@ -6,9 +6,9 @@ def write_and_analyze(ds__, out_file__, batch_size__,
                       take_size__, cum_batch__, keys__):
   cums = [[], [], [], [], []]
   analysis_matrix = [[], [], [], []]
-  cum_data = [[] for _ in keys__]
+  cum_data = [0 for _ in keys__]
   cum_batch = 0
-
+  write_incidents = 0
   for m, data_elem in enumerate(ds__.batch(batch_size__, 
                                                      drop_remainder=True)
                                 .take(take_size__)):
@@ -16,16 +16,22 @@ def write_and_analyze(ds__, out_file__, batch_size__,
       cums[i].append(stat)
     cums[-1].append(data_elem['dist'])
 
-    for i, key in enumerate(keys__):
-      cum_data[i].append(data_elem[key])
+    for i, key in enumerate([i.split('.')[0] for i in keys__]):
+      if type(cum_data[i]) == int:
+        cum_data[i] = data_elem[key]
+      else:
+        cum_data[i] = tf.concat([cum_data[i], data_elem[key]], axis=0)
 
     cum_batch += 1
 
-    if cum_batch % cum_batch__ == 0:
-      mynbm.dataset_utils.io.write_tfr_from_list(cum_data, keys__, out_file__)
-      cum_data = [[] for _ in keys__]
+    if cum_batch %  cum_batch__ == 0:
+      write_incidents += 1
+      mynbm.dataset_utils.io.write_tfr_from_list(cum_data, 
+                                                 keys__, 
+                                                 out_file__ + f'{write_incidents}.tfr')
+      cum_data = [0 for _ in keys__]
 
-  for i, cum in enumerate(cums):
+  for i, cum in enumerate(cums[:-1]):
     my_cum = tf.convert_to_tensor(cum)
     my_cum = tf.reshape(tf.transpose(my_cum, perm=[2,0,1]), shape=(2,-1))
     analysis_matrix[i].append(tfm.reduce_min(my_cum, axis=1))
@@ -34,7 +40,7 @@ def write_and_analyze(ds__, out_file__, batch_size__,
     analysis_matrix[i].append(tfm.reduce_std(my_cum, axis=1))
     
   analysis_matrix = [analysis_matrix]
-  mynbm.dataset.utils.io.write_tfr_from_list(analysis_matrix,
+  mynbm.dataset_utils.io.write_tfr_from_list(analysis_matrix,
                                             ['analysis.f32'],
                                              out_file__ + '-analysis.tfr')
 
