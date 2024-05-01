379e26a6758aa593de727983c915ad2fc9103ab9
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-05-01 19:13:19 +0000

    Fancy-ing the plot

--- my_notebook_modules/misc/nb3/plot.py
+++ my_notebook_modules/misc/nb3/plot.py
@@ -30,17 +30,31 @@ def heatmap(analysis_matrix__):
   ax[1].set_title('Imaginary Values Matrix')
   plt.show()
 
-def distance_dist(distances__):
+def distance_dist(distances__, bins__):
   f, ax = plt.subplots(ncols=2, figsize=(12,5))
   b = scipy.stats.gaussian_kde(distances__)
   plt.tight_layout(pad=3)
 
   x = np.arange(0, 300, .1)
+  xtick = np.arange(0, 300, 25)
   ax[0].set_title('Histogram')
-  ax[0].hist(distances__, bins=20, color='black')
+  ax[0].hist(distances__, bins=bins__, color='black')
   ax[0].set_ylabel('Frequency')
+
+  max_dist = tfm.reduce_max(distances__)
+  min_dist = tfm.reduce_min(distances__)
+  h_length = (max_dist - min_dist) / bins__
+
+  text_info = f'Max Distance: {max_dist:.2f} km\n'
+  text_info += f'Min Distance: {min_dist:.2f} km\n'
+  text_info += f'Hist Length: {h_length:.2f} km'
+
+  ax[0].text(0.95, 0.95, text_info, 
+             va='top', ha='right',
+             transform=ax[0].transAxes)
   ax[1].set_title('KDE Dist.')
-  ax[1].set_xlabel('Distances')
+  ax[0].set_xlabel('Distances')
+  ax[0].set_xticks(xtick)
   ax[1].plot(x, b(x), color='black', label='KDE graph of distance distribution')
   ax[1].axvline(tfm.reduce_mean(distances__), color='red', label='Mean')
   ax[1].set_ylabel('Density')
