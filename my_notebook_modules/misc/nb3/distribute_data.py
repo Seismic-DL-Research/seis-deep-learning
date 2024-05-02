1ebe3893bc3e165476d4e9410f505b8ce6e5e9a0
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-05-02 11:06:55 +0000

    previous update was intricate! Simplifying process

--- my_notebook_modules/misc/nb3/distribute_data.py
+++ my_notebook_modules/misc/nb3/distribute_data.py
@@ -2,11 +2,12 @@ import tensorflow as tf
 import my_notebook_modules as mynbm
 import my_notebook_modules.dataset_utils.op.dataset_operation as mynbm_dsop
 
-def distribute_data(dataset__, dataset_keys__, key__, unit__, hist_length__, 
-                    bot_range__, top_range__, threshold__, tolerance__, out_file__,
-                    take_size__, batches_per_file__=-1):
+def distribute_data(dataset__, dataset_keys__, take_size__, batches_per_file__,
+hist__, hist_length__, key__):
   curr_val = 0
-  while curr_val < top_range__:
+  frequencies, bins = hist__
+  
+  for i in range(0, len(bins)):
     curr_val += hist_length__
     uniformed_ds = dataset__.filter(mynbm_dsop.filterFunc_specific_key_range(
       bot_range__=curr_val - hist_length__,
