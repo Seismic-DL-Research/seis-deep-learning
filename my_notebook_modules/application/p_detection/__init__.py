25c3f24d0c378dcce2b67ade7879167a241b482e
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-05-26 13:40:07 +0700

    .

--- my_notebook_modules/application/p_detection/__init__.py
+++ my_notebook_modules/application/p_detection/__init__.py
@@ -1,3 +1,6 @@
 from .gan1 import gan1
 from .gan3 import gan3
 from .complexgan import complexgan
+from .gan1_predict import *
+from .gan3_predict import *
+from .complexgan_predict import *
---
+++ my_notebook_modules/application/p_detection/gan3_predict.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+def single():
+  pass
+
+def sliding():
+  pass
+
+def batch():
+  pass
