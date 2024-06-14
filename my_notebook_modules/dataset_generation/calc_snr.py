1dfd850d5f75236429bcc252e15996148dd0d3a2
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-06-14 21:44:30 +0700

    .

--- my_notebook_modules/dataset_generation/calc_snr.py
+++ my_notebook_modules/dataset_generation/calc_snr.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def calc_snr(noise_data, signal_data):
-  rms_noisex = np.sqrt(np.mean(noise_data**2))
+  rms_noise = np.sqrt(np.mean(noise_data**2))
   rms_signal = np.sqrt(np.mean(signal_data**2))
   return 20 * np.log(rms_signal / rms_noise) / np.log(10)
