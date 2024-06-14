36be6a56f3069cc376703b80ed65785f3c04e1df
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-06-14 21:41:21 +0700

    .

--- my_notebook_modules/dataset_generation/__init__.py
+++ my_notebook_modules/dataset_generation/__init__.py
@@ -1,4 +1,4 @@
 from .calc_haversine import calc_haversine
 from .fetch_extract_and_list import fetch_extract_and_list
 from .generate import generate
-from .calc_stalta import stalta
+from .calc_stalta import calc_stalta
