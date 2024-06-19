1721e2da205a858c20b12cfe85c8f6d4431c896b
Raihansyah-for-Capstone <rai.machine.learning.explore@gmail.com>
2024-06-19 12:31:58 +0700

    .

--- my_notebook_modules/dataset_generation/calc_stalta.py
+++ my_notebook_modules/dataset_generation/calc_stalta.py
@@ -4,7 +4,7 @@ import numpy as np
 #@njit(fastmath=True)
 def calc_stalta(waveform_data, nsta, nlta, trigger):
   # this is a better approach for STA/LTA
-  cumsum = np.cumsum(np.abs(waveform_data))
+  cumsum = np.cumsum(waveform_data)
   sta = np.concatenate((cumsum[:nsta], cumsum[nsta:] - cumsum[:-nsta]))
   lta = np.concatenate((cumsum[:nlta], cumsum[nlta:] - cumsum[:-nlta]))
   sta[:nlta] = 0
